f77c28814fa2983950b81adafd25fe4319b9e142
geoyee <geoyee@yeah.net>
2024-12-10 14:10:33 +0800

    feat: Init add

---
+++ hypara.hpp
@@ -0,0 +1,182 @@
+#pragma once
+
+#ifndef _HYPARA_HPP_
+#define _HYPARA_HPP_
+
+#pragma warning(disable : 4239)
+#pragma warning(disable : 4996)
+
+#include <chrono>
+#include <functional>
+#include <future>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
+namespace hyp
+{
+template<typename T>
+class Task;
+
+template<typename Ret, typename... Args>
+class Task<Ret(Args...)>
+{
+public:
+    using return_type = Ret;
+
+    Task(std::function<Ret(Args...)>&& fn) : m_fn(std::move(fn)) { }
+
+    Task(std::function<const Ret(Args...)>& fn) : m_fn(fn) { }
+
+    ~Task() = default;
+
+    std::shared_future<Ret> run(Args&&...args)
+    {
+        return std::async(m_fn, std::forward<Args>(args)...);
+    }
+
+    void wait(Args&&...args)
+    {
+        std::async(m_fn, std::forward<Args>(args)...).wait();
+    }
+
+    Ret get(Args&&...args)
+    {
+        return std::async(m_fn, std::forward<Args>(args)...).get();
+    }
+
+    template<typename Func>
+    auto then(Func&& fn) -> Task<typename std::result_of<Func(Ret)>::type(Args...)>
+    {
+        using result_type = typename std::result_of<Func(Ret)>::type;
+
+        auto func = std::move(m_fn);
+        auto task = Task<result_type(Args...)>(
+            [func, &fn](Args... args)
+            {
+                std::future<Ret> lastFunc = std::async(func, std::forward<Args>(args)...);
+                return std::async(fn, lastFunc.get()).get();
+            });
+
+        return task;
+    }
+
+private:
+    std::function<Ret(Args...)> m_fn;
+};
+
+namespace aux
+{
+template<typename Ret>
+struct RangeTrait
+{
+    using type = Ret;
+};
+
+template<typename Ret>
+struct RangeTrait<std::shared_future<Ret>>
+{
+    using type = Ret;
+};
+
+template<typename Range, typename... Args>
+auto transform(Range& range,
+               std::tuple<Args...> tArgs) -> std::vector<std::shared_future<typename Range::value_type::return_type>>
+{
+    using result_type = typename Range::value_type::return_type;
+    std::vector<std::shared_future<result_type>> funcs;
+    for (auto& task : range)
+    {
+        funcs.emplace_back(std::apply([&task](auto&&...args) { return task.run(std::forward<Args>(args)...); }, tArgs));
+    }
+    return funcs;
+}
+
+template<typename Range>
+auto getAnyResultPair(Range& funcs) -> std::pair<size_t, typename RangeTrait<typename Range::value_type>::type>
+{
+    size_t count = funcs.size();
+    while (true)
+    {
+        for (size_t i = 0; i < count; ++i)
+        {
+            if (funcs[i].wait_for(std::chrono::milliseconds(1)) == std::future_status::ready)
+            {
+                return std::make_pair(i, funcs[i].get());
+            }
+        }
+    }
+}
+
+template<typename Func,
+         typename Range,
+         std::enable_if_t<std::is_invocable_r<bool, Func, typename RangeTrait<typename Range::value_type>::type>::value,
+                          bool> = true>
+auto getOnlyResultPair(Func& cfn,
+                       Range& funcs) -> std::pair<size_t, typename RangeTrait<typename Range::value_type>::type>
+{
+    size_t count = funcs.size();
+    while (true)
+    {
+        for (size_t i = 0; i < count; ++i)
+        {
+            if (funcs[i].wait_for(std::chrono::milliseconds(1)) == std::future_status::ready)
+            {
+                auto res = funcs[i].get();
+                if (cfn(res))
+                {
+                    return std::make_pair(i, std::move(res));
+                }
+            }
+        }
+    }
+}
+} // namespace aux
+
+template<typename Range, typename... Args>
+inline static auto All(Range& range, Args&&...args) -> Task<std::vector<typename Range::value_type::return_type>()>
+{
+    using result_type = typename Range::value_type::return_type;
+
+    return [&range, tArgs = std::tuple<Args...>(std::forward<Args>(args)...)]() mutable
+    {
+        std::vector<std::shared_future<result_type>> funcs;
+        for (auto& task : range)
+        {
+            funcs.emplace_back(
+                std::apply([&task](auto&&...args) { return task.run(std::forward<Args>(args)...); }, std::move(tArgs)));
+        }
+        size_t count = funcs.size();
+        std::vector<result_type> res(count);
+        for (size_t i = 0; i < count; ++i)
+        {
+            res[i] = funcs[i].get();
+        }
+        return res;
+    };
+}
+
+template<typename Range, typename... Args>
+inline static auto Any(Range& range,
+                       Args&&...args) -> Task<std::pair<size_t, typename Range::value_type::return_type>()>
+{
+    return [&range, tArgs = std::tuple<Args...>(std::forward<Args>(args)...)]() mutable
+    { return aux::getAnyResultPair(aux::transform(range, tArgs)); };
+}
+
+template<typename Func, typename Range, typename... Args>
+inline static auto Only(Func&& fn,
+                        Range& range,
+                        Args&&...args) -> Task<std::pair<size_t, typename Range::value_type::return_type>()>
+{
+    return [&fn, &range, tArgs = std::tuple<Args...>(std::forward<Args>(args)...)]()
+    { return aux::getOnlyResultPair(fn, aux::transform(range, tArgs)); };
+}
+} // namespace hyp
+
+#define HypTask hyp::Task
+#define HypAll  hyp::All
+#define HypAny  hyp::Any
+#define HypOnly hyp::Only
+
+#endif // !_HYPARA_HPP_
---
+++ main.cpp
@@ -0,0 +1,142 @@
+#include "hypara.hpp"
+
+#include <iostream>
+#include <cmath>
+#include <thread>
+#include <numeric>
+#include <random>
+
+int main()
+{
+    // Init
+    std::random_device rnd;
+    std::mt19937 rGen(rnd());
+    std::cout.precision(8);
+
+    // Task base
+    {
+        HypTask<double(double)> task([](double x) -> double { return std::sqrt(x); });
+        double res = task.get(8.0);
+        std::cout << "sqrt(8.0) = " << res << std::endl;
+    }
+    // All, Any and Only
+    {
+        std::vector<HypTask<double(double)>> tasks{
+            HypTask<double(double)>(
+                [&rGen](double x) -> double
+                {
+                    auto start = std::chrono::high_resolution_clock::now();
+                    if (x > 10)
+                    {
+                        return -1;
+                    }
+                    std::uniform_real_distribution<> tDis(0.1, 1);
+                    std::uniform_real_distribution<> eDis(-0.08, 0.08);
+                    double t = tDis(rGen);
+                    double e = eDis(rGen);
+                    std::this_thread::sleep_for(std::chrono::milliseconds((int)(t * 100)));
+                    auto end = std::chrono::high_resolution_clock::now();
+                    std::chrono::duration<double, std::milli> elapsed = end - start;
+                    std::cout << "[Index 1]: use " << elapsed.count() << "ms" << std::endl;
+                    return std::sqrt(x) + e;
+                }),
+            HypTask<double(double)>(
+                [&rGen](double x) -> double
+                {
+                    auto start = std::chrono::high_resolution_clock::now();
+                    if (x > 50)
+                    {
+                        return -1;
+                    }
+                    std::uniform_real_distribution<> tDis(1, 9.9);
+                    std::uniform_real_distribution<> eDis(-0.0009, 0.0009);
+                    double t = tDis(rGen);
+                    double e = eDis(rGen);
+                    std::this_thread::sleep_for(std::chrono::milliseconds((int)(t * 100)));
+                    auto end = std::chrono::high_resolution_clock::now();
+                    std::chrono::duration<double, std::milli> elapsed = end - start;
+                    std::cout << "[Index 2]: use " << elapsed.count() << "ms" << std::endl;
+                    return std::sqrt(x) + e;
+                }),
+            HypTask<double(double)>(
+                [&rGen](double x) -> double
+                {
+                    auto start = std::chrono::high_resolution_clock::now();
+                    if (x > 100)
+                    {
+                        return -1;
+                    }
+                    std::uniform_real_distribution<> tDis(10, 19.9);
+                    std::uniform_real_distribution<> eDis(-0.00002, 0.00002);
+                    double t = tDis(rGen);
+                    double e = eDis(rGen);
+                    std::this_thread::sleep_for(std::chrono::milliseconds((int)(t * 100)));
+                    auto end = std::chrono::high_resolution_clock::now();
+                    std::chrono::duration<double, std::milli> elapsed = end - start;
+                    std::cout << "[Index 3]: use " << elapsed.count() << "ms" << std::endl;
+                    return std::sqrt(x) + e;
+                }),
+            HypTask<double(double)>(
+                [&rGen](double x) -> double
+                {
+                    auto start = std::chrono::high_resolution_clock::now();
+                    std::uniform_real_distribution<> tDis(20, 50);
+                    std::uniform_real_distribution<> eDis(-0.00000000000001, 0.00000000000001);
+                    double t = tDis(rGen);
+                    double e = eDis(rGen);
+                    std::this_thread::sleep_for(std::chrono::milliseconds((int)(t * 100)));
+                    auto end = std::chrono::high_resolution_clock::now();
+                    std::chrono::duration<double, std::milli> elapsed = end - start;
+                    std::cout << "[Index 4]: use " << elapsed.count() << "ms" << std::endl;
+                    return std::sqrt(x) + e;
+                })};
+
+        auto start = std::chrono::high_resolution_clock::now();
+        double ave = HypAll(tasks, 8.0)
+                         .then(
+                             [](std::vector<double>& res)
+                             {
+                                 std::cout << "every result is: ";
+                                 for (const auto& r : res)
+                                 {
+                                     std::cout << r << ", ";
+                                 }
+                                 double ave = (double)(std::accumulate(res.begin(), res.end(), 0.0) / res.size());
+                                 return ave;
+                             })
+                         .get();
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double, std::milli> elapsed = end - start;
+        std::cout << "sqrt(8.0) = " << ave << " --> [" << elapsed.count() << "ms]" << std::endl;
+
+        start = std::chrono::high_resolution_clock::now();
+        double fast = HypAny(tasks, 8.0)
+                          .then(
+                              [](std::pair<size_t, double>& res)
+                              {
+                                  std::cout << "fast task index = " << (int)res.first << " and ";
+                                  return res.second;
+                              })
+                          .get();
+        end = std::chrono::high_resolution_clock::now();
+        elapsed = end - start;
+        std::cout << "sqrt(8.0) = " << fast << " --> [" << elapsed.count() << "ms]" << std::endl;
+
+        constexpr double rel = 8.9442719099991587856366946749251;
+        auto check = [&rel](double x) -> bool { return std::abs(x - rel) < 1e-3; };
+        start = std::chrono::high_resolution_clock::now();
+        double only = hyp::Only(check, tasks, 80.0)
+                          .then(
+                              [](std::pair<size_t, double>& res)
+                              {
+                                  std::cout << "only task index = " << (int)res.first << " and ";
+                                  return res.second;
+                              })
+                          .get();
+        end = std::chrono::high_resolution_clock::now();
+        elapsed = end - start;
+        std::cout << "sqrt(80.0) = " << only << " --> [" << elapsed.count() << "ms]" << std::endl;
+    }
+
+    return 0;
+}
